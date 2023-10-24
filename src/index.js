cc398b247fa412c0819d814b695f90dea0122982
John Mo <peikaimo@gmail.com>
2023-10-24 13:23:33 -0500

    testing home

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Hashrouter as Router, Route, Routes } from "react-router-dom";
 
 // components
 import {
