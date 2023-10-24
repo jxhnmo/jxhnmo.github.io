485b7da3a12b6dc53b459b7d50e532ad61451d5b
John Mo <peikaimo@gmail.com>
2023-10-24 17:31:30 -0500

    attempt to fix blank

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ ReactDOM.render(
   <Router basename={process.env.PUBLIC_URL}>
     <Navbar />
     <Routes>
-      <Route path="/jxhnmo.github.io/" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/links" element={<Links />} />
       <Route path="/experience" element={<Experience />} />
