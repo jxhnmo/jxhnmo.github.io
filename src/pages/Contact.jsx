253babc04a850db6cf13bfd7630adfafa02c6b67
John Mo <peikaimo@gmail.com>
2023-11-01 22:16:12 -0500

    :added resume password page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,59 @@
 import React from "react";
+import * as Form from '@radix-ui/react-form';
+// import './Contact.css';
 
 function Contact() {
   return (
-    <div className="contact">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Contact</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
+    <div id="page-container" class="my-3">
+      <h2>Let's Talk!</h2>
+      <div class="main">
+        <div class="row lightContainer">
+          <Form.Root className="FormRoot" action="mailto:peikaimo@gmail.com" method="post">
+
+            <div class="section">
+              <Form.Field className="FormField" name="name">
+                <Form.Label className="FormLabel">Name</Form.Label>
+                <Form.Control class="Input"
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  placeholder="Enter your name"
+                />
+              </Form.Field>
+
+              <Form.Field className="FormField" name="email">
+                <Form.Label className="FormLabel">Email</Form.Label>
+                <Form.Control class="Input"
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  placeholder="Enter your email"
+                />
+              </Form.Field>
+            </div>
+
+            <div class="section">
+              <Form.Field className="FormField" name="message">
+                <Form.Control asChild>
+                  <textarea className="Textarea" placeholder="What's up?" required />
+                </Form.Control>
+              </Form.Field>
+            </div>
+
+            <div class="section">
+              <Form.Submit asChild>
+                <button type="submit" className="Button" style={{ marginTop: 10 }}>
+                  Send
+                </button>
+              </Form.Submit>
+            </div>
+          </Form.Root>
+
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
