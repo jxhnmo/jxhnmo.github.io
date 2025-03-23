bca4621c32af148b0ce8c08a44d78d54b93d2866
John Mo <peikaimo@gmail.com>
2025-03-23 17:18:54 -0500

    updated work experience to include powerdb

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import * as Accordion from '@radix-ui/react-accordion';
-import classNames from 'classnames';
-import { ChevronDownIcon } from '@radix-ui/react-icons';
-import './Experience.css';
+import * as Accordion from "@radix-ui/react-accordion";
+import classNames from "classnames";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import "./Experience.css";
 
-import samsclub from '../assets/work/samsclub.png'
-import supersocial from '../assets/work/supersocial_inc_logo.jpeg'
-import mercyships from '../assets/work/Mercy_Ships_Logo.jpg'
-import unt from '../assets/work/unt.png'
+import powerdb from "../assets/work/powerdb.png";
+import samsclub from "../assets/work/samsclub.png";
+import supersocial from "../assets/work/supersocial_inc_logo.jpeg";
+import mercyships from "../assets/work/Mercy_Ships_Logo.jpg";
+import unt from "../assets/work/unt.png";
 
 function Experience() {
   return (
@@ -15,8 +16,37 @@ function Experience() {
       <h2>Experience</h2>
       <div class="main">
         <div class="row lightContainer">
+          <Accordion.Root
+            className="AccordionRoot"
+            type="single"
+            defaultValue=""
+            collapsible
+          >
+            <Accordion.Item className="AccordionItem" value="item-5">
+              <AccordionTrigger>
+                <div class="date">2025-Present</div>
+                <div class="name">PowerDB</div>
+                <div class="title">Software Developer</div>
+                <div class="location">College Station, Texas</div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <div class="companyContainer">
+                  <div class="left">Part time developer.</div>
 
-          <Accordion.Root className="AccordionRoot" type="single" defaultValue="" collapsible>
+                  <div class="right">
+                    <th id="logo">
+                      <img
+                        src={powerdb}
+                        alt="powerdb logo"
+                        width="100%"
+                        height="auto"
+                      />
+                    </th>
+                  </div>
+                </div>
+              </AccordionContent>
+            </Accordion.Item>
+
             <Accordion.Item className="AccordionItem" value="item-4">
               <AccordionTrigger>
                 <div class="date">2023-2024</div>
@@ -27,20 +57,34 @@ function Experience() {
               <AccordionContent>
                 <div class="companyContainer">
                   <div class="left">
-                    <li>Owned two initiatives targeting online and in-club associate fraud; authored PRDs that outlined strategic solutions, user stories, and product roadmap; aligned cross-functional teams; actively unblocked engineering dependencies.</li><br />
-                    <li>Developed and implemented data driven rules, resulting in a 60% reduction in online agent collusion fraud.</li><br />
-                    <li>Impacted bottom line by $3.6M in annualised estimated fraud cost savings.</li>
+                    <li>
+                      Reduced online agent collusion fraud by 60%, preventing
+                      $3.6M in annualised estimated fraud losses by developing
+                      and implementing data-driven rules.
+                    </li>
+                    <br />
+                    <li>
+                      Owned two initiatives targeting online and in-club
+                      associate fraud; authored PRDs that outlined strategic
+                      solutions, user stories, and product roadmap; aligned
+                      cross-functional teams; actively unblocked engineering
+                      dependencies.
+                    </li>
                   </div>
 
                   <div class="right">
                     <th id="logo">
-                      <img src={samsclub} alt='samsclub logo' width="100%" height='auto' />
+                      <img
+                        src={samsclub}
+                        alt="samsclub logo"
+                        width="100%"
+                        height="auto"
+                      />
                     </th>
                   </div>
                 </div>
               </AccordionContent>
             </Accordion.Item>
-
 
             <Accordion.Item className="AccordionItem" value="item-3">
               <AccordionTrigger>
@@ -53,14 +97,32 @@ function Experience() {
                 <div class="companyContainer">
                   <div class="left">
                     Built the foundation of Supersocial's Fortnite department.
-                    <li>Created a fully-fledged brand IP prototype using UEFN/Verse and Agile methodology, handling game design, programming, balance adjustments, and arranging playtests</li><br />
-                    <li>Authored entire technical documentation tree on Fortnite/UEFN/Verse with 50+ Verse scripts/functionality tutorials.</li><br />
-                    <li>Presented prototypes and documentation at company all-hands and answered ad-hoc questions from PMs and designers.</li>
+                    <li>
+                      Created a fully-fledged brand IP prototype using
+                      UEFN/Verse and Agile methodology, handling game design,
+                      programming, balance adjustments, and arranging playtests
+                    </li>
+                    <br />
+                    <li>
+                      Authored a comprehensive technical documentation tree with
+                      50+ Verse scripts/tutorials
+                    </li>
+                    <br />
+                    <li>
+                      Presented prototypes and documentation at company
+                      all-hands and answered ad-hoc questions from PMs and
+                      designers.
+                    </li>
                   </div>
 
                   <div class="right">
                     <th id="logo">
-                      <img src={supersocial} alt='supersocial logo' width="100%" height='auto' />
+                      <img
+                        src={supersocial}
+                        alt="supersocial logo"
+                        width="100%"
+                        height="auto"
+                      />
                     </th>
                   </div>
                 </div>
@@ -77,13 +139,27 @@ function Experience() {
               <AccordionContent>
                 <div class="companyContainer">
                   <div class="left">
-                    <li>Built full-stack applications that automated travel applications for housing, flight, medical, and managerial checks, and more, increasing global operational efficiency.</li><br />
-                    <li>Resolved 100+ support tickets for Microsoft 365 and Atlassian Suite.</li>
+                    <li>
+                      Built full-stack applications that automated travel
+                      applications for housing, flight, medical, and managerial
+                      checks, and more, increasing global operational
+                      efficiency.
+                    </li>
+                    <br />
+                    <li>
+                      Resolved 100+ support tickets for Microsoft 365 and
+                      Atlassian Suite, ensuring seamless technical operations.
+                    </li>
                   </div>
 
                   <div class="right">
                     <th id="logo">
-                      <img src={mercyships} alt='mercyships logo' width="100%" height='auto' />
+                      <img
+                        src={mercyships}
+                        alt="mercyships logo"
+                        width="100%"
+                        height="auto"
+                      />
                     </th>
                   </div>
                 </div>
@@ -100,49 +176,57 @@ function Experience() {
               <AccordionContent>
                 <div class="companyContainer">
                   <div class="left">
-                    <li>Built XR environments in Unity and wrote C# scripts to run psychological tests under the guidance of Dr. Thomas D. Parsons and Dr. Timothy (Fred) McMahan.</li><br />
-                    <li>Acquired a $4000 grant.</li>
+                    <li>
+                      Secured a $4000 grant and developed XR environments in
+                      Unity, implementing C# scripts for psychological testing.
+                    </li>
                   </div>
 
                   <div class="right">
                     <th id="logo">
-                      <img src={unt} alt='unt logo' width="100%" height='auto' />
+                      <img
+                        src={unt}
+                        alt="unt logo"
+                        width="100%"
+                        height="auto"
+                      />
                     </th>
                   </div>
                 </div>
               </AccordionContent>
             </Accordion.Item>
           </Accordion.Root>
-
-
         </div>
       </div>
     </div>
   );
 }
 
-const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
-  <Accordion.Header className="AccordionHeader">
-    <Accordion.Trigger
-      className={classNames('AccordionTrigger', className)}
+const AccordionTrigger = React.forwardRef(
+  ({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Header className="AccordionHeader">
+      <Accordion.Trigger
+        className={classNames("AccordionTrigger", className)}
+        {...props}
+        ref={forwardedRef}
+      >
+        {children}
+        <ChevronDownIcon className="AccordionChevron" aria-hidden />
+      </Accordion.Trigger>
+    </Accordion.Header>
+  )
+);
+
+const AccordionContent = React.forwardRef(
+  ({ children, className, ...props }, forwardedRef) => (
+    <Accordion.Content
+      className={classNames("AccordionContent", className)}
       {...props}
       ref={forwardedRef}
     >
-      {children}
-      <ChevronDownIcon className="AccordionChevron" aria-hidden />
-    </Accordion.Trigger>
-  </Accordion.Header>
-));
-
-const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
-  <Accordion.Content
-    className={classNames('AccordionContent', className)}
-    {...props}
-    ref={forwardedRef}
-  >
-    <div className="AccordionContentText">{children}</div>
-  </Accordion.Content>
-));
-
+      <div className="AccordionContentText">{children}</div>
+    </Accordion.Content>
+  )
+);
 
 export default Experience;
